66aff2cc4c4a8eb7b536ccee1a03dd72161fd088
SK Manjur Ali <skmanjuralli9@gmail.com>
2022-12-13 21:18:09 +0530

    add to git

---
+++ src/main/java/com/example/RestApiController/UserController.java
@@ -0,0 +1,37 @@
+package com.example.RestApiController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController //spring knows we are making rest API
+public class UserController {
+    @Autowired
+    UserService userService;
+    // database
+
+    @GetMapping("/get_users")
+    public ResponseEntity<List<User>> getUsers(){
+
+        return new ResponseEntity(userService.getAllUsers(), HttpStatus.ACCEPTED);
+    }
+
+//    @PostMapping("/add_user")
+//    public void addUser(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("age") int age,
+//                        @RequestParam("country") String country){
+//
+//        User user = new User(id,name,country,age);
+//        users.put(id,user);
+//    }
+
+    @PostMapping("/add_user_body")
+    public ResponseEntity addUserBody(@RequestBody(required = true)User u)
+    {
+        userService.addUserToDB(u);
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+}
